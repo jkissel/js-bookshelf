2830dc71c40fa840f5a610832f4539daa8cab0ee
Michael Mueller <haimich@gmx.net>
2013-02-19 09:35:11 +0100

    ...

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 #!/usr/bin/node
 
+var fs = require('fs');
+var configFile = 'conf/config.json';
+
 var express = require('express')
 var app = express();
 
 var util = require('util'),
     OperationHelper = require('apac').OperationHelper;
 
-var opHelper = new OperationHelper({
-    awsId:     'AKIAIFMXF2YDHSUITRBA',
-    awsSecret: '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',
-    assocId:   null
-});
+//read config from file
+var config = fs.readFileSync(configFile, 'UTF-8');
+var aws_id, aws_secret;
+if (config) {
+  config = JSON.parse(config);
+  aws_id = config.awsId;
+  aws_secret = config.awsSecret;
 
-app.get("/test", function(req, res) {
-  res.end("test");
+  if (!aws_id || !aws_secret) {
+    throw new Error("Either awsId or awsSecret could not be read from config file");
+  }
+} else {
+  throw new Error("File " + configFile + " could not be read");
+}
+
+var opHelper = new OperationHelper({
+    awsId:     aws_id,
+    awsSecret: aws_secret,
+    assocId:   null
 });
 
 app.get("/search", function(req, res) {
