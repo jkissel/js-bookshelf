da5402c47d860dbad1b6268d60659ff3b95e1610
Michael Müller <haimich@gmx.net>
2013-02-21 21:59:20 +0100

    fixed problem with server crashes; added different apac library; implemented search to return images and link

--- server.js
+++ server.js
@@ -1,55 +1,119 @@
 #!/usr/bin/node
 
+"use strict";
+
 var fs = require('fs');
 var configFile = 'conf/config.json';
 
-var express = require('express')
+var express = require('express');
 var app = express();
 
-var util = require('util'),
-    OperationHelper = require('apac').OperationHelper;
+var util = require('util');
+var OperationHelper = require('json-apac').OperationHelper;
+var encoding = 'UTF-8';
 
-//read config from file
-var config = fs.readFileSync(configFile, 'UTF-8');
-var aws_id, aws_secret;
-if (config) {
-  config = JSON.parse(config);
-  aws_id = config.awsId;
-  aws_secret = config.awsSecret;
-
-  if (!aws_id || !aws_secret) {
-    throw new Error("Either awsId or awsSecret could not be read from config file");
-  }
-} else {
-  throw new Error("File " + configFile + " could not be read");
-}
+var config = readConfigFromFile(configFile, encoding);
 
 var opHelper = new OperationHelper({
-    awsId:     aws_id,
-    awsSecret: aws_secret,
-    assocId:   null
+  awsId:     config.awsId,
+  awsSecret: config.awsSecret,
+  assocId:   null
 });
 
+/*
+ *******************
+ * Server
+ *******************
+*/
+
+//search for a text string and return all matches with images
 app.get("/search", function(req, res) {
   var text = req.query.text;
   
   opHelper.execute('ItemSearch', {
     'SearchIndex': 'Books',
     'Keywords': text,
-    'ResponseGroup': 'ItemAttributes'
-  }, function(error, results) {
-    if (error) {
-      console.log('Error: ' + error + '\n');
+    'ResponseGroup': 'ItemAttributes,Images'
+  }, function(statusCode, response) {
+    if (statusCode) {
+      console.log('Error: ' + statusCode + '\n');
       res.end();
+    } else {
+      var obj = JSON.parse(response).ItemSearchResponse.Items;
+      var output = "", imgUrl = "";
+
+      if (!obj.Request.IsValid) {
+        console.log('Invalid request: ' + obj.Request + '\n');
+        res.end();
+      } else {
+        output = obj.Item.map(function(item) {
+          imgUrl = (item.MediumImage === undefined ? '' : item.MediumImage.URL);
+
+          return '<li><a href="' + item.DetailPageURL + '">' + 
+            item.ItemAttributes.Title + '</a> (' + 
+            item.ASIN + ') --&gt; ' +
+            '<img src="' + imgUrl + '" /></li>';
+        });
+
+        res.setHeader("Content-Type", "text/html");        
+        res.end('<html><head><title>Result</title></head><body>\n' +
+          '<h1>Found items:</h1>\n' + 
+          '<ul>' + output.join('<br />\n') + 
+          '</ul>\n' +
+          '</body></html>'
+          , encoding);
+      }
     }
-    
-    var ASINs = results.ItemSearchResponse.Items[0].Item.map(function(item) {
-      return item.ASIN;
-    });
-    
-    res.end('Found ASINs:\n' + ASINs.join('\n') + '\n');
+  });
+});
+
+//get a specific item by ASIN id and return the image
+app.get("/search/item/:id", function(req, res) {
+  var id = req.params.id;
+  
+  opHelper.execute('ItemLookup', {
+    'ItemId': id,
+    'IdType': 'ASIN',
+    'ResponseGroup': 'Images'
+  }, function(statusCode, response) {
+    if (statusCode) {
+      console.log('Error: ' + statusCode + '\n');
+      res.end();
+    } else {
+      var obj = JSON.parse(response).ItemLookupResponse.Items;
+      var imgUrl = "";
+
+      if (!obj.Request.IsValid) {
+        console.log('Invalid request: ' + obj.Request + '\n');
+        res.end();
+      } else {
+        imgUrl = (obj.Item.MediumImage === undefined ? '' : obj.Item.MediumImage.URL);
+        res.end(imgUrl, encoding);
+      }
+    }
   });
 });
 
 app.listen(3000);
 
+/*
+ *******************
+ * Utility methods
+ *******************
+*/
+
+function readConfigFromFile(configFile, encoding) {
+  var fileContent = fs.readFileSync(configFile, encoding);
+  var fileAsObj;
+
+  if (fileContent) {
+    fileAsObj = JSON.parse(fileContent);
+
+    if (!fileAsObj.awsId || !fileAsObj.awsSecret) {
+      throw new Error("Either awsId or awsSecret could not be read from the config file");
+    }
+  } else {
+    throw new Error("File " + configFile + " could not be read");
+  }
+  return fileAsObj;
+}
